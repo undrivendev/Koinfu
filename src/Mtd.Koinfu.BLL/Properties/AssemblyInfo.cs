46943bd639d61c9e5f9ab18cd3a7a7bd4c3be118
Luca Dalla Valle <lucadv91@gmail.com>
2018-12-01 02:12:07 +0100

    added version endpoint

---
+++ src/Mtd.Koinfu.BLL/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Mtd.Koinfu.BLL")]
+[assembly: AssemblyDescription("Koinfu Business Logic Layer")]
+[assembly: AssemblyCompany("Methodically Driven Soft")]
+[assembly: AssemblyProduct("Mtd.Koinfu.BLL")]
+[assembly: AssemblyCopyright("Copyright © 2018 - 2019")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
---
+++ src/Mtd.Koinfu.DAL/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Mtd.Koinfu.DAL")]
+[assembly: AssemblyDescription("Koinfu Data Access Layer")]
+[assembly: AssemblyCompany("Methodically Driven Soft")]
+[assembly: AssemblyProduct("Mtd.Koinfu.DAL")]
+[assembly: AssemblyCopyright("Copyright © 2018 - 2019")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
